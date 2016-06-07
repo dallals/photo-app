f5f3fd041a2d919462b6f471e9ca71b1eb0fdf33
Sammy Dallal <sammydallal@gmail.com>
2016-06-07 13:17:10 -0700

    Minor updates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,10 @@
 class WelcomeController < ApplicationController
 	skip_before_action :authenticate_user!, only: [:index]
   def index
-  	# @instagram = Image.all
-  	# @images = Image.paginate(page: params[:page], per_page: 10).order(updated_at: :desc)
   	# @instagram = Instagram.media_popular
   	# @instagram = Instagram.user_recent_media(9326124, {:count => 18})
   	@images = Image.all.order(updated_at: :desc)
-  	# @results = Google::Search::News.new(query: "President Obama")
+  	# @images = Image.find(current_user.id).order(updated_at: :desc)
 
   end
 end
