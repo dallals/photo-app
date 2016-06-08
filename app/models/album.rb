a8dcd089ec25573850d9deb90d7edc21a8066da3
Sammy Dallal <sammydallal@gmail.com>
2016-06-07 23:57:08 -0700

    Fixed some validations for Albums and Images

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,7 +2,6 @@ class Album < ActiveRecord::Base
   has_many :image_albums
   has_many :images, through: :image_albums
   # has_many :images
-  validates :name, presence: true, length: { mainmum: 3, maximum: 25 }
-  # validates_uniqueness_of :name
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   belongs_to :user
 end
