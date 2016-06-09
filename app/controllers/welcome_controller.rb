3a916ca28afc5cabee299abf6912c79102373422
Sammy Dallal <sammydallal@gmail.com>
2016-06-08 20:25:20 -0700

    hopefully fixed current_users top 8 pics

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,10 @@ class WelcomeController < ApplicationController
   def index
   	# @instagram = Instagram.media_popular
   	# @instagram = Instagram.user_recent_media(9326124, {:count => 18})
-  	@images = Image.all.order(updated_at: :desc)
+  	# @images = Image.all.order(updated_at: :desc)
+  	# @images = Image.joins(:user).where(:users => {:user_id => current_user.id })
   	# @images = Image.find(current_user.id).order(updated_at: :desc)
-
+  	# @comments = Comment.joins(:post).where(:posts => { :user_id => @user.id })
+  	@images = Image.where(user_id: current_user)
   end
 end
