68d351c9d275360dd84b4bb66d8924fb9409e533
Sammy Dallal <sammydallal@gmail.com>
2016-06-24 20:55:32 -1000

    code clean up

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,6 @@ class WelcomeController < ApplicationController
   def index
   	# @instagram = Instagram.media_popular
   	# @instagram = Instagram.user_recent_media(9326124, {:count => 18})
-  	# @images = Image.all.order(updated_at: :desc)
   	@images = Image.where(user_id: current_user).order(updated_at: :desc)
   end
 end
