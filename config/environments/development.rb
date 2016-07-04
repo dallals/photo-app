53454968f2d782c0a48e93f639b0acdf1c05d60b
Sammy Dallal <sammydallal@gmail.com>
2016-07-03 20:01:18 -0700

    404

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'http://localhost:3000'}
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  # config.consider_all_requests_local       = false
+  # config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
