ec7e0f82e152bef86d72ae60a0f01a0ebc52f784
Sammy Dallal <sammydallal@gmail.com>
2016-07-04 12:48:55 -0700

    got s3 working

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,8 +2,9 @@ if Rails.env.development?
 	CarrierWave.configure do |config|
 		config.fog_credentials = {
 			:provider => 'AWS',
-			:aws_access_key_id => ENV['aws_access_key_id'],
-			:aws_secret_access_key => ENV['aws_secret_access_key']
+			:aws_access_key_id => ENV['AWS_access_key_id'],
+			:aws_secret_access_key => ENV['AWS_secret_access_key'],
+			:region                 => 'us-west-1'
 		}
 
 		config.fog_directory = ENV['AWS_BUCKET']
