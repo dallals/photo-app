33024123efe5809463e6fac74ae401d61ac281cf
Sammy Dallal <sammydallal@gmail.com>
2017-04-15 22:15:47 -0700

    carrierwave to production

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,17 +1,4 @@
-if Rails.env.development?
-	CarrierWave.configure do |config|
-		config.fog_credentials = {
-			:provider => 'AWS',
-			:aws_access_key_id => ENV['AWS_access_key_id'],
-			:aws_secret_access_key => ENV['AWS_secret_access_key'],
-			:region                => 'us-west-1'
-		}
-
-		config.fog_directory = ENV['AWS_BUCKET']
-	end
-end
-
-# if Rails.env.production?
+# if Rails.env.development?
 # 	CarrierWave.configure do |config|
 # 		config.fog_credentials = {
 # 			:provider => 'AWS',
@@ -23,3 +10,16 @@ end
 # 		config.fog_directory = ENV['AWS_BUCKET']
 # 	end
 # end
+
+if Rails.env.production?
+	CarrierWave.configure do |config|
+		config.fog_credentials = {
+			:provider => 'AWS',
+			:aws_access_key_id => ENV['AWS_access_key_id'],
+			:aws_secret_access_key => ENV['AWS_secret_access_key'],
+			:region                => 'us-west-1'
+		}
+
+		config.fog_directory = ENV['AWS_BUCKET']
+	end
+end
